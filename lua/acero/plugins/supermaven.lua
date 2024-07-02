67f3bf30e3ad8c9f64d26bf0f4227392cd536377
Diego Acero <diegoaceroarguelles@gmail.com>
2024-07-02 08:35:06 +0200

    Configure supermaven and remove copilot from used plugins

--- lua/acero/plugins/supermaven.lua
+++ lua/acero/plugins/supermaven.lua
@@ -1,6 +1,12 @@
 return {
   "supermaven-inc/supermaven-nvim",
   config = function()
-    require("supermaven-nvim").setup({})
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<C-a>",
+        clear_suggestion = "<A-q>",
+        accept_word = "<C-w>",
+      },
+    })
   end,
 }
